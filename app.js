a100f3de338de2d7d74e254deb8efc9b9b255d40
Clover Chapman <clo@cloverseeds.co.uk>
2018-04-15 17:53:08 +0100

    Adding an activity to a user - step3 completed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const createActivity = require('./controllers/createActivity');
 
 // instantiate express
 const app = express();
+
+app.post('/profile/activities', createActivity);
 
 app.listen(3000, () => console.log('Microlife app listening on port 3000!'));
